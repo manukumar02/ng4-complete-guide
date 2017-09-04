da5276d6e3411083ac2c485b7079592666e066fd
Manu Sharma <manukumar02@gmail.com>
2017-09-04 11:26:53 +0530

    [Model] - created a model for data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Taste Recipe', 'This is simply a test', 'http://www.seriouseats.com/recipes/assets_c/2017/06/20170523-ladyfingers-vicky-wasik-19-thumb-1500xauto-437954.jpg'),
+    new Recipe('A Taste Recipe', 'This is simply a test', 'http://www.seriouseats.com/recipes/assets_c/2017/06/20170523-ladyfingers-vicky-wasik-19-thumb-1500xauto-437954.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
